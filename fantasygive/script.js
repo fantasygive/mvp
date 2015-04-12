10aed9992fcceb81cbfdce36318fdc127bc25206
Cameron Luck <cameron.t.luck@gmail.com>
2015-04-11 22:52:17 -0400

    MVP Reset
    Commented broken code out. Working on passing username from “Login”
    screen to “Scoreboard” screen.

--- fantasygive/script.js
+++ fantasygive/script.js
@@ -1,25 +1,25 @@
 angular.module('ionicApp', ['ionic'])
 
-    .run(function($ionicPlatform, $rootScope, $state, userService) {
-        $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-            // for form inputs)
-            if(window.cordova && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-            }
-            if(window.StatusBar) {
-                StatusBar.styleDefault();
-            }
-        });
-        // UI Router Authentication Check
-        $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
-            if (toState.data.authenticate && !userService.isLoggedIn()){
-                // User isn’t authenticated
-                $state.transitionTo("login");
-                event.preventDefault();
-            }
-        });
-    })
+    //.run(function($ionicPlatform, $rootScope, $state, userService) {
+    //    $ionicPlatform.ready(function() {
+    //        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    //        // for form inputs)
+    //        if(window.cordova && window.cordova.plugins.Keyboard) {
+    //            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    //        }
+    //        if(window.StatusBar) {
+    //            StatusBar.styleDefault();
+    //        }
+    //    });
+    //    // UI Router Authentication Check
+    //    $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams){
+    //        if (toState.data.authenticate && !userService.isLoggedIn()){
+    //            // User isn’t authenticated
+    //            $state.transitionTo("login");
+    //            event.preventDefault();
+    //        }
+    //    });
+    //})
 
     .config(function($stateProvider, $urlRouterProvider) {
 
@@ -27,10 +27,10 @@ angular.module('ionicApp', ['ionic'])
     .state('signin', {
       url: "/sign-in",
       templateUrl: "sign-in.html",
-      controller: 'SignInCtrl',
-          data: {
-              authenticate: false
-          }
+      controller: 'SignInCtrl'
+          //data: {
+          //    authenticate: false
+          //}
     })
     .state('forgotpassword', {
       url: "/forgot-password",
@@ -47,9 +47,9 @@ angular.module('ionicApp', ['ionic'])
         'scoreboard-tab': {
           templateUrl: "scoreboard.html",
           controller: 'ScoreboardTabCtrl',
-            data: {
-                authenticate: true
-            }
+            //data: {
+            //    authenticate: true
+            //}
         }
       }
     })
@@ -81,50 +81,62 @@ angular.module('ionicApp', ['ionic'])
    $urlRouterProvider.otherwise("/sign-in");
 
 })
+    //
+    //.factory('userService', ['$rootScope', '$state', function($rootScope, $state) {
+    //
+    //    // Hello.js Functions
+    //    hello.init({
+    //            // replace this with your own Facebook App ID
+    //            yahoo : 'dj0yJmk9ZlYzTVJYQ2Jod1hiJmQ9WVdrOWIwOHpSRzR5TkhNbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1kYw--'
+    //        });
+    //
+    //    var service = {
+    //        isLoggedIn: function() {
+    //            return $rootScope.userStatus;
+    //        },
+    //        login: function() {
+    //            hello('yahoo').login( function() {
+    //                token = hello( 'yahoo' ).getAuthResponse().access_token;
+    //                console.log(token); //code breaks right after this point
+    //                hello( 'yahoo' ).api( '/me' ).success(function(json) {
+    //                    console.log(json);
+    //                    $rootScope.user = json;
+    //                    $rootScope.$apply($rootScope.user);
+    //                    $rootScope.userStatus = true;
+    //
+    //                    $state.go('home');
+    //                });
+    //            });
+    //        },
+    //        logout: function() {
+    //            hello('yahoo').logout( function() {
+    //                $rootScope.userStatus = false;
+    //                $rootScope.user = null;
+    //
+    //                $state.go('login');
+    //            });
+    //        }
+    //    }
+    //
+    //    return service;
+    //}])
 
-    .factory('userService', ['$rootScope', '$state', function($rootScope, $state) {
+    //.controller('ScoreboardTabCtrl', ['$scope', 'userService',function($scope, userService) {
+    //    $scope.logout = userService.logout;
+    //}])
+    //
+    //.controller('SignInCtrl', ['$scope', 'userService', function($scope, userService) {
+    //    $scope.login = userService.login;
+    //}]);
 
-        // Hello.js Functions
-        hello.init({
-                // replace this with your own Facebook App ID
-                yahoo : 'dj0yJmk9ZlYzTVJYQ2Jod1hiJmQ9WVdrOWIwOHpSRzR5TkhNbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1kYw--'
-            });
 
-        var service = {
-            isLoggedIn: function() {
-                return $rootScope.userStatus;
-            },
-            login: function() {
-                hello('yahoo').login( function() {
-                    token = hello( 'yahoo' ).getAuthResponse().access_token;
-                    console.log(token); //code breaks right after this point
-                    hello( 'yahoo' ).api( '/me' ).success(function(json) {
-                        console.log(json);
-                        $rootScope.user = json;
-                        $rootScope.$apply($rootScope.user);
-                        $rootScope.userStatus = true;
+    .controller('SignInCtrl', function ($scope, $state) {
+        $scope.signIn = function (user) {
+            console.log('Sign-In', user);
+            $state.go('tabs.scoreboard');
+        };
+    })
 
-                        $state.go('home');
-                    });
-                });
-            },
-            logout: function() {
-                hello('yahoo').logout( function() {
-                    $rootScope.userStatus = false;
-                    $rootScope.user = null;
-
-                    $state.go('login');
-                });
-            }
-        }
-
-        return service;
-    }])
-
-    .controller('ScoreboardTabCtrl', ['$scope', 'userService',function($scope, userService) {
-        $scope.logout = userService.logout;
-    }])
-
-    .controller('SignInCtrl', ['$scope', 'userService', function($scope, userService) {
-        $scope.login = userService.login;
-    }]);
+    .controller('ScoreboardTabCtrl', function ($scope) {
+        console.log('ScoreboardTabCtrl');
+    });
